c92631db589b11f3c2b0a72ecbdd31771e02911c
swapnilkadam18 <kadam.swapnil09@gmail.com>
2016-09-17 10:07:05 +0530

    Added activities and respective layout methods

---
+++ app/src/main/java/com/android/swapnil/fragactlifecycle/Activity_B.java
@@ -0,0 +1,15 @@
+package com.android.swapnil.fragactlifecycle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by swapnil on 17/09/2016.
+ */
+public class Activity_B extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/android/swapnil/fragactlifecycle/Util.java
@@ -0,0 +1,13 @@
+package com.android.swapnil.fragactlifecycle;
+
+import android.util.Log;
+
+/**
+ * Created by swapnil on 17/09/2016.
+ */
+public class Util {
+
+    public static void printOnConsole(String TAG, String message){
+        Log.e(TAG,message);
+    }
+}
